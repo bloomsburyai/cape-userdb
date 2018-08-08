786e7210c89ae8781cad6593f1535574d7e8e0ec
Mike Sheldon <mike@bloomsbury.ai>
2018-08-08 14:31:04 +0100

    Add user tests

--- cape_userdb/tests/test_userdb.py
+++ cape_userdb/tests/test_userdb.py
@@ -1,5 +1,48 @@
-from cape_userdb.create_tables import create_tables
+from cape_userdb.user import User
+from peewee import IntegrityError
+import pytest
 
 
-def test_userdb():
-    create_tables()
+def test_user_creation():
+    try:
+        del_user = User.get('user_id', 'fake-id')
+        del_user.delete_instance()
+    except:
+        pass
+    user = User(user_id='fake-id', password='test')
+    user.save()
+    test_user = User.get('user_id', 'fake-id')
+    assert user == test_user
+    user.delete_instance()
+
+
+def test_unique_user():
+    try:
+        del_user = User.get('user_id', 'fake-id')
+        del_user.delete_instance()
+    except:
+        pass
+
+    user = User(user_id='fake-id', password='test')
+    user.save()
+    with pytest.raises(IntegrityError):
+        duplicate_user = User(user_id='fake-id', password='test')
+        duplicate_user.save()
+
+    user.delete_instance()
+
+
+def test_delete_user():
+    try:
+        del_user = User.get('user_id', 'fake-id')
+        del_user.delete_instance()
+    except:
+        pass
+
+    user = User(user_id='fake-id', password='test')
+    user.save()
+
+    user.delete_instance()
+
+    test_user = User.get('user_id', 'fake-id')
+    assert test_user is None
